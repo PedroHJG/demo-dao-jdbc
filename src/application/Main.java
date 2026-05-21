a2ca860a823ba379ce29a9af03f1b3a9637ff9c1
pedrogois <phjgoiis@gmail.com>
2026-05-21 16:37:49 -0300

    Seller created

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,16 @@
 package application;
 
 import model.entities.Departament;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Departament dp = new Departament(123, "Financeiro");
 
+        Seller sl = new Seller(123,"Pedro", "pedro@gmail.com", new Date(), 20000.0, dp);
         System.out.println(dp);
+        System.out.println(sl);
     }
 }
