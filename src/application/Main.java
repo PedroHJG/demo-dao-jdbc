4953bde4ed5c03119d3df92af41b5a769143306c
pedrogois <phjgoiis@gmail.com>
2026-05-21 16:22:14 -0300

    departament created

---
+++ src/application/Main.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.entities.Departament;
+
+public class Main {
+    public static void main(String[] args) {
+        Departament dp = new Departament(123, "Financeiro");
+
+        System.out.println(dp);
+    }
+}
